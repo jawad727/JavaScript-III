a9c2683e89bc3176acfc70ddf73923e512b3fef5
Jawad Hussein <jawaddhussein@gmail.com>
2019-01-23 17:05:46 -0800

    MVP Complete

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. Global Binding: In the global scope, the value of this will point to the window object
 * 2. Implicit Binding: Whatever is to the left of the dot becomes the context for this in the function
-* 3. New binding: This, when used before a key (this.name = name;) will point to the object name
+* 3. New binding: Refers to the specific object that is created by a constructor function
 * 4. Explicit Binding: Allows the developer to choose what to bind this to by using (call(), bind(), apply())
 *
 
@@ -17,6 +17,8 @@
 // code example for Window Binding
 console.log(this);
 
+
+
 // Principle 2
 
 // code example for Implicit Binding
@@ -28,14 +30,28 @@ const jawadObj = {
     }
 };
 
+
+
 // Principle 3
 
 // code example for New Binding
+
+function Alien(name) {
+    this.name = name;
+    this.greeting = 'Greetings earthling, we come in peace';
+    this.speak = function() {
+      console.log(this.greeting);
+    };
+  }
+  
+  const ET = new Alien('ET');
+  
 
 
 // Principle 4
 
 // code example for Explicit Binding
+
 const Person = {
     name: "john"
 }
